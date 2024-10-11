8a894349bdaf1914a34f71b9673b4a56be08be71
bgonc <52862119+bgonc@users.noreply.github.com>
2024-10-11 22:10:30 +0300

    update

--- somadigitos.c
+++ somadigitos.c
@@ -24,4 +24,28 @@ int main()
 
     }
 
+    /*    minha solução:
+    
+    int n;
+    int i;
+    int soma;
+
+    printf("Calculo da soma do quadrado dos digitos de um numero: \n");
+    printf("Numero: ");
+    scanf("%d", &n);
+
+    while (0<n)
+
+    {
+        i=(n%10)*(n%10);
+
+        soma+=i;
+
+        printf("n=%d; soma=%d\n", n, soma);
+
+        n=n/10;
+
+    }
+    printf("Resultado: %d\n", soma);*/
+
 }
--- teste.c
+++ teste.c
@@ -3,32 +3,24 @@
 int main()
 {
     int n;
-    int r;
-    int i;
-    int aux;
-    
-    printf("Calculo dos arranjos de N, R a R:\n");
-    printf("Indique N: ");
+    int i=3;
+    int soma;
+    int fib1=1;
+    int fib2=2;
+
+    printf("Calculo do valor da funcao Fibonacci:\nIndique N: ");
     scanf("%d", &n);
-    printf("Indique R: ");
-    scanf("%d", &r);
     
-    if(r<n && r>0)
-        {
+    while(i<=n)
+    {
     
-            i=n-r+1;
-            aux=1;
-            
-            while(i<=n)
-            {
-            
-                aux*=i;
-                printf("i=%d; arranjos=%d\n", i, aux);
-                i++;
-            }
-            printf("Resultado=%d\n", aux);
-        }
-    else
+    soma=fib1+fib2;
     
-        printf("Erro: N tem de ser maior que R e este maior que 0.");
+    printf("Fib(%d)=%d\n", i, soma);
+    
+    fib1=fib2;
+    fib2=soma;
+
+    i++;
+    }   
 }
