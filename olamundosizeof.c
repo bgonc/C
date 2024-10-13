a7d9f45b82731b5f1cfc7cf8afa15596a74cc352
bgonc <52862119+bgonc@users.noreply.github.com>
2024-10-13 17:00:32 +0300

    test

--- olamundosizeof.c
+++ olamundosizeof.c
@@ -4,14 +4,14 @@ int main()
 
 {
     printf("Olá Mundo!");
-    printf("\nsizeof(char): %zu", sizeof(char));
-    printf("\nsizeof(short): %zu", sizeof(short));
-    printf("\nsizeof(int): %zu", sizeof(int));
-    printf("\nsizeof(long): %zu", sizeof(long));
-    printf("\nsizeof(long long): %zu", sizeof(long long));
-    printf("\nsizeof(float): %zu", sizeof(float));
-    printf("\nsizeof(double): %zu", sizeof(double));
-    printf("\nsizeof(double long): %zu", sizeof(long double));
+    printf("\nsizeof(char): %d", sizeof(char));
+    printf("\nsizeof(short): %d", sizeof(short));
+    printf("\nsizeof(int): %d", sizeof(int));
+    printf("\nsizeof(long): %d", sizeof(long));
+    printf("\nsizeof(long long): %d", sizeof(long long));
+    printf("\nsizeof(float): %d", sizeof(float));
+    printf("\nsizeof(double): %d", sizeof(double));
+    printf("\nsizeof(double long): %d", sizeof(long double));
 
-    printf("\nO total é %zu", sizeof(short)+sizeof(int)+sizeof(long)+sizeof(long long)+sizeof(float)+sizeof(double)+sizeof(long double));
+    printf("\nO total é %d", sizeof(short)+sizeof(int)+sizeof(long)+sizeof(long long)+sizeof(float)+sizeof(double)+sizeof(long double));
 }
--- teste.c
+++ teste.c
@@ -2,25 +2,21 @@
 
 int main()
 {
-    int n;
-    int i=3;
-    int soma;
-    int fib1=1;
-    int fib2=2;
 
-    printf("Calculo do valor da funcao Fibonacci:\nIndique N: ");
-    scanf("%d", &n);
-    
-    while(i<=n)
-    {
-    
-    soma=fib1+fib2;
-    
-    printf("Fib(%d)=%d\n", i, soma);
-    
-    fib1=fib2;
-    fib2=soma;
+    int h;
+    int m;
+    int s;
+    int hms;
 
-    i++;
-    }   
+    printf("Calculo do numero de segundos desde o inicio do dia.\n");
+    printf("hora\n");
+    scanf("%d", &h);
+    printf("minuto\n");
+    scanf("%d", &m);
+    printf("segundo\n");
+    scanf("%d", &s);
+
+    hms=(((h*60)*60)+(m*60)+s);
+
+    printf("Total: %d", hms);
 }
