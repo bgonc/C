fbed44d8ec64946aa8cc5e7e756f3cfc5d4a4b16
bgonc <52862119+bgonc@users.noreply.github.com>
2024-10-16 17:31:33 +0300

    tripla

--- primo.c
+++ primo.c
@@ -1,36 +1,27 @@
 #include <stdio.h>
 
-int main()
-
+int main() 
 {
-    int n;
-    int i=1;
-    int resto=1;
-    int zero=0;
+    int n, i = 2, menor_divisor = 0;
 
-    printf("Funcao que verifica se um numero N e' primo\nIndique N:\n");
+    printf("Indique N:\n");
     scanf("%d", &n);
 
-    
-    /*if(n%n == 0 && n%1 == 0 && n%i == 0)*/
-
-    while(i<=n)
-    {   
-        resto=n%i;
-        i++;
-o 
-        if(resto == 0)
+    while (i * i <= n) 
+    {
+        if (n % i == 0) 
         {
-            zero++;
-            if(zero == 2)
-                printf("%d\n %d ", resto, zero);
-            
-            else
-                printf("%d\n %d", resto, zero);
-        
+            menor_divisor = i;  
+            i = n;
         }
-
+        i++;
     }
 
+    if (menor_divisor == 0 && n > 1) 
+    {
+        menor_divisor = 0;
+    }
+
+    printf("%d\n", menor_divisor);
 
 }
