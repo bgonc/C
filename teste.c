7c0b5f7605ea47ded9f7e49c6d679a871552d49c
bgonc <52862119+bgonc@users.noreply.github.com>
2024-10-31 21:41:55 +0200

    TESTE

--- teste.c
+++ teste.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
-
-int main()
-
+ 
+int main(int argc, char *argv[])
 {
-    double n = 1;
-    double f = 1;
-    int k;
-    int i = 1;
-
-    printf("indique K: ");
-    scanf("%d", &k);
-
-    while (i <= k)
-    {
-        f *= i;
-        n += 1 / f;
-
-        printf("%d: %.16g\n", i, n);
-
-        i++;
-    }
+    printf("Argumentos: %d\nArgumento 0: %s", argc, argv[0]);
+    
 }
