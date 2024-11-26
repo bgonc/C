06f41cceee4da26e5e82dae7bfbda93540a076bd
bgonc <52862119+bgonc@users.noreply.github.com>
2024-11-26 22:45:05 +0200

    sort

--- ATV2/sort.c
+++ ATV2/sort.c
@@ -2,40 +2,49 @@
 
 unsigned int randaux()
 {
-    static long seed=1;
-  return(((seed = seed * 214013L + 2531011L) >> 16) & 0x7fff);
+  static long seed = 1;
+  return (((seed = seed * 214013L + 2531011L) >> 16) & 0x7fff);
 }
 
-int PrintInts(int v[], int n)
+void PrintInts(int v[], int n)
 {
-    for(int i = 0; i < n; i++)
-        printf("%d ", v[i]);
-        
+  for (int i = 0; i < n; i++)
+    printf("%d ", v[i]);
 }
 
-int Sort(int v[], int n)
+void Sort(int v[], int n)
 {
-    int aux;
+  for (int i = 0; i < n - 1; i++)
+  {
 
-    for(int i = 0; i < n; i++)
-        
+    for (int j = i + 1; j < n; j++)
+    {
+      if (v[i] > v[j])
+      {
 
+        int aux = v[i];
+        v[i] = v[j];
+        v[j] = aux;
+      }
+    }
+  }
 }
 
 int main()
 {
-    int v[1000],i, n;
+  int v[1000], i, n;
 
-    scanf("%d",&n);
-    n++;
+  scanf("%d", &n);
+  n++;
 
-    for(i=0;i<1000;i++)
-		v[i]=randaux()%n;
+  for (i = 0; i < 1000; i++)
+    v[i] = randaux() % n;
 
-	PrintInts(v,10);
+  PrintInts(v, 10);
 
-	Sort(v,1000);
+  printf("\n");
 
+  Sort(v, 1000);
 
-    
+  PrintInts(v, 10);
 }
--- teste.c
+++ teste.c
@@ -1,7 +1,61 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+unsigned int randaux()
 {
-    printf("Argumentos: %d\nArgumento 0: %s", argc, argv[0]);
-    
+    static long seed = 1;
+    seed = seed * 214013L + 2531011L;
+    return ((seed >> 16) & 0x7fff);
+}
+
+void PrintInts(int v[], int n, const char* sep)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", v[i]);
+        if (i < n - 1)
+            printf(" ");
+    }
+    printf("%s", n);
+}
+
+void Sort(int v[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Compare with all subsequent elements
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[i] > v[j])
+            {
+                // Swap elements if they are in reverse order
+                int temp = v[i];
+                v[i] = v[j];
+                v[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int v[1000], i, n;
+
+    // Read the maximum random number
+    scanf("%d", &n);
+    n++;
+
+    // Fill the array with random numbers from 0 to N
+    for (i = 0; i < 1000; i++)
+        v[i] = randaux() % n;
+
+    // Print the first 10 numbers before sorting
+    PrintInts(v, 10, "\n");
+
+    // Sort the array
+    Sort(v, 1000);
+
+    // Print the first 10 numbers after sorting
+    PrintInts(v, 10, "\n");
+
+    return 0;
 }
