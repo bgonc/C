c21d7517d49d456a3113c3f75b5a43af2533cc2f
bgonc <52862119+bgonc@users.noreply.github.com>
2024-12-19 14:58:10 +0200

    vs code linux

--- arranjos.c
+++ arranjos.c
@@ -6,29 +6,29 @@ int main()
     int r;
     int i;
     int aux;
-    
+
     printf("Calculo dos arranjos de N, R a R:\n");
     printf("Indique N: ");
     scanf("%d", &n);
     printf("Indique R: ");
     scanf("%d", &r);
-    
-    if(r<n && r>0)
+
+    if (r < n && r > 0)
+    {
+
+        i = n - r + 1;
+        aux = 1;
+
+        while (i <= n)
         {
-    
-            i=n-r+1;
-            aux=1;
-            
-            while(i<=n)
-            {
-            
-                aux*=i;
-                printf("i=%d; arranjos=%d\n", i, aux);
-                i++;
-            }
-            printf("Resultado=%d\n", aux);
+
+            aux *= i;
+            printf("i=%d; arranjos=%d\n", i, aux);
+            i++;
         }
+        printf("Resultado=%d\n", aux);
+    }
     else
-    
+
         printf("Erro: N tem de ser maior que R e este maior que 0.");
 }
