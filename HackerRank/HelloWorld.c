dea28b478809f9d0d4a79651a23cbafbc2769824
bgonc <bgonc@users.noreply.github.com>
2025-02-27 23:49:43 +0200

    1

--- HackerRank/HelloWorld.c
+++ HackerRank/HelloWorld.c
@@ -10,8 +10,6 @@ int main()
     scanf("%[^\n]%*c", &s);
 
     printf("Hello, World!\n%s", s);
-
-  	
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+  
     return 0;
 }
