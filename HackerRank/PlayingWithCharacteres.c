f648765f20885062e9dcfab410f4815769ba1c13
bgonc <bgonc@users.noreply.github.com>
2025-02-27 23:39:54 +0200

    testes

---
+++ HackerRank/PlayingWithCharacteres.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char ch;
+    //char s[];
+    //char sen[];
+
+    scanf("%c", &ch);
+
+    printf("%c\n", ch);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
