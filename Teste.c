e14bf52ffed0ef1d754593b2a1451c51af71658d
Bruno Goncalves <52862119+bgonc@users.noreply.github.com>
2025-04-27 19:55:41 +0300

    small fixes

--- Teste.c
+++ Teste.c
@@ -1,31 +1,28 @@
-
 #include <stdio.h>
 // teste de commit
 
-long long int power(int n, int p)
-{
-    long long int  resultado = 1;
-    
-    while (p > 0)
-	{
-	    resultado *= n;
-	    p--;
-	}
-    return resultado;
+long long int power(int n, int p) {
+  long long int resultado = 1;
+
+  while (p > 0) {
+    resultado *= n;
+    p--;
+  }
+  return resultado;
 }
 
-int main()
-{
-    int num;
-    int pow;
+int main() {
+  int num;
+  int pow;
+  int total;
 
-    printf("Indique o número:");
-    scanf("%d", &num);
-    printf("Indique a potência:");
-    scanf("%d", &pow);
+  printf("Indique o número:");
+  scanf("%d", &num);
+  printf("Indique a potência:");
+  scanf("%d", &pow);
 
-    printf("%d elevado a %d = %lld\n",num, pow, power(num, pow));
+  printf("%d elevado a %d = %lld\n", num, pow, power(num, pow));
 
-    return 0;
+  return 0;
 }
 // mais um teste
