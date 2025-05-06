1876ecf5fd031257e972ae543591a0d1faf64199
Bruno Goncalves <52862119+bgonc@users.noreply.github.com>
2025-05-06 20:42:30 +0300

    teste magit

--- Teste.c
+++ Teste.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
+#include <string.h>
 
-#define COUNT 5
+#define NUM_LIVROS 3
 
-int main() {
+typedef struct {
+    char titulo[100];
+    char autor[50];
+    int ano;
+    int paginas;
+} Livro;
 
-    int values[COUNT] = {5, 10, 15, 20, 25};
-    int totalSum = 0;
+int main(void) {
 
-    float averageValue;
+    Livro biblioteca[NUM_LIVROS];
 
-    for (int i = 0; i < COUNT; i++) {
-        totalSum += values[i];
+    for (int i = 0; i < NUM_LIVROS; i++) {
+        printf("Indique o titulo do livro %d: ", i + 1);
+        fgets(biblioteca[i].titulo, sizeof(biblioteca[i].titulo), stdin);
+        biblioteca[i].titulo[strcspn(biblioteca[i].titulo, "\n")] = '\0';
+
+        printf("Indique o autor do livro %d: ", i + 1);
+        fgets(biblioteca[i].autor, sizeof(biblioteca[i].autor), stdin);
+        biblioteca[i].autor[strcspn(biblioteca[i].autor, "\n")] = '\0';
+
+        printf("Indique o ano do livro %d: ", i + 1);
+        scanf("%d", &biblioteca[i].ano);
+
+        printf("Indique o numero de páginas do livro %d: ", i + 1);
+        scanf("%d", &biblioteca[i].paginas);
+
+        while (getchar() != '\n')
+            ;
     }
-    printf("Sum: %d\n", totalSum);
 
-    averageValue = (float)totalSum / COUNT;
-
-    printf("Average: %.2f\n", averageValue);
-
+    for (int i = 0; i < NUM_LIVROS; i++) {
+        if (biblioteca[i].paginas > 200) {
+            printf("Título: %s\nAutor: %s\nAno: %d\nPáginas: %d", biblioteca[i].titulo, biblioteca[i].autor, biblioteca[i].ano, biblioteca[i].paginas);
+        }
+    }
     return 0;
 }
