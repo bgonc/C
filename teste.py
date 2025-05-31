936abe7063fa0a593c7468debc0c10c2c8d8abae
Bruno Goncalves <52862119+bgonc@users.noreply.github.com>
2025-05-31 22:52:07 +0300

    py

---
+++ teste.py
@@ -0,0 +1,14 @@
+print("hahaah")
+
+def teste():
+
+    print("teste")
+    return "teste" 
+if __name__ == "__main__":
+    print("teste")
+    teste()  
+    print("teste")
+    print("teste")
+
+
+    
